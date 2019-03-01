85385a44c52a70c4b9d091dcb169d4ec69dbbb6e
janecowles <janemcowles@gmail.com>
2019-03-01 10:58:20 -0700

    fixed e248 process

--- Code/KK_Code.R
+++ Code/KK_Code.R
@@ -141,9 +141,9 @@ e247$NAdd <- ifelse(e247$NTrt == 0, 0,1) # Need NAdd column to indicate N additi
 e248 <- e248[,c(1:4,6)]
 names(e248) <- c("Year", "Plot", "NTrt", "Water", "SR")
 e248$NTrt <- as.character(e248$NTrt)
-e248[e248$NTrt == "A",] <- 0
-e248[e248$NTrt == "B",] <- 7
-e248[e248$NTrt == "C",] <- 14
+e248$NTrt[e248$NTrt == "A"] <- 0 #this was e248[e248$...] which changed everything to 0 7 or 14, i changed to e248$NTrt[e248$...] so just changes NTrt
+e248$NTrt[e248$NTrt == "B"] <- 7
+e248$NTrt[e248$NTrt == "C"] <- 14
 e248$NTrt <- as.numeric(e248$NTrt)
 e248$Exp <- "e248"
 e248$TrtYear <- e248$Year - min(e248$Year) + 1
@@ -169,49 +169,3 @@ write.csv(dat.fin, here::here("Data", "findat.csv"))
 # Subplot and block - designated for some experiments
 # Water - were the plots irrigated; 0,1 (NA if not a treatment)
 
-
-
-###########################################################################
-## CAN PROBABLY DELETE BELOW HERE vvv
-
-# Step 1: Find magnitude and direction of effect in year 1, 3 and 10 separately
-
-
-# load data
-
-# NewNTrt A = ambient; B = +7 g/m2/year; C = +14 g/m2/year
-# WaterTrt 0 = ambient; 1 = ~ 1 inch/week/growing season
-
-# Get columns that we need (Year, Treatment (Irrigation or N addition), and specie richness)
-e248 <- e248[,c(1:4,6)]
-# Rename columns
-names(e248) <- c("Year", "Plot", "NTrt", "H2OTrt", "SR")
-# Add experiment year column
-e248$ExpYear <- e248$Year-2006
-
-# Right now we will look at the magnitude and direction of change within each year only
-# We may want to consider fitting a temporal trend and using the predicted values
-# So we can account for year to year variation in the plots from climatic variables, too.
-
-mod1 <- lm(SR ~ NTrt*H2OTrt, data = e248[e248$ExpYear ==1, ]) # All Interactions
-summary(mod1)
-mod2 <- lm(SR ~ NTrt + H2OTrt, data = e248[e248$ExpYear ==1, ]) # No interaction
-summary(mod2)
-## Conclusions: There is no significant difference between ambient and treated plots
-## in the first year 
-## It seems like we gain species in the first year in the High N and Irrigated plots
-
-mod3 <- lm(SR ~ NTrt*H2OTrt, data = e248[e248$ExpYear ==3, ]) # All Interactions
-summary(mod3)
-mod4 <- lm(SR ~ NTrt + H2OTrt, data = e248[e248$ExpYear ==3, ]) # No interaction
-summary(mod4)
-
-
-mod5 <- lm(SR ~ NTrt*H2OTrt, data = e248[e248$ExpYear ==10, ]) # All Interactions
-summary(mod5)
-mod6 <- lm(SR ~ NTrt + H2OTrt, data = e248[e248$ExpYear ==10, ]) # No interaction
-summary(mod6)
-# Here N treatments significantly reduce species richness.
-
-## THOUGHTS: SHould we pull all the Estimated values to put in another model? 
-## OR - should we model everything together to begin with?
